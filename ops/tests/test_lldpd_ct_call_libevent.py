d265da806df954e291919dc15bbf0370dac8107c
Neeta <neeta.bhat@hp.com>
2015-09-30 21:57:05 -0400

    pep8 changes for CT
    Change-Id: I4f3e87004adc5672b7701d3e3b2b877555b42fd7
    Signed-off-by: Neeta <neeta.bhat@hp.com>

--- ops/tests/test_lldpd_ct_call_libevent.py
+++ ops/tests/test_lldpd_ct_call_libevent.py
@@ -38,41 +38,42 @@ from opsvsiutils.systemutil import *
 # S1[1]
 #
 
+
 class myTopo(Topo):
 
-    def build (self, hsts=2, sws=2, **_opts):
+    def build(self, hsts=2, sws=2, **_opts):
 
         self.hsts = hsts
         self.sws = sws
-
 
         # Add switches
         for s in irange(1, sws):
             switch = self.addSwitch('s%s' % s)
 
+
 class lldpTest (OpsVsiTest):
 
-    def setupNet (self):
+    def setupNet(self):
 
         # if you override this function, make sure to
         # either pass getNodeOpts() into hopts/sopts of the topology that
         # you build or into addHost/addSwitch calls
-        self.net = Mininet(topo=myTopo(hsts = 0, sws = 1,
-                                       hopts = self.getHostOpts(),
-                                       sopts = self.getSwitchOpts()),
-                                       switch = VsiOpenSwitch,
-                                       host = Host,
-                                       link = OpsVsiLink,
-                                       controller = None,
-                                       build = True)
+        self.net = Mininet(topo=myTopo(hsts=0, sws=1,
+                                       hopts=self.getHostOpts(),
+                                       sopts=self.getSwitchOpts()),
+                           switch=VsiOpenSwitch,
+                           host=Host,
+                           link=OpsVsiLink,
+                           controller=None,
+                           build=True)
 
-    def switch_variable (self, switch_number):
+    def switch_variable(self, switch_number):
         if (switch_number == 1):
             return self.net.switches[0]
         else:
             return self.net.switches[1]
 
-    def configure_lldp (self, switch_number):
+    def configure_lldp(self, switch_number):
         info("\n")
         info("### enabling lldp globally for switch %d ###\n" % switch_number)
         s = self.switch_variable(switch_number)
@@ -80,42 +81,47 @@ class lldpTest (OpsVsiTest):
             self.pid1 = s.cmd("pidof ops-lldpd").strip()
             assert self.pid1 != "", "lldp process not running on switch " + \
                 str(switch_number)
-            info("### lldp process id on switch " + \
-                str(switch_number) + \
-                " is " + \
-                str(self.pid1) + " ###\n")
+            info("### lldp process id on switch " +
+                 str(switch_number) +
+                 " is " +
+                 str(self.pid1) + " ###\n")
         else:
             assert switch_number == 1, "Invalid switch " + \
-                           str(switch_number)
-        uuid = s.cmd("ovs-vsctl list system | grep _uuid | " + \
+                str(switch_number)
+        uuid = s.cmd("ovs-vsctl list system | grep _uuid | " +
                      "awk '{print $3}'").strip()
         info("### got openvswitch uuid - %s ###\n" % uuid)
-        s.cmd("ovs-vsctl -t 30 set system %s " \
-              "other_config:lldp_enable=true " \
+        s.cmd("ovs-vsctl -t 30 set system %s "
+              "other_config:lldp_enable=true "
               "other_config:lldp_tx_interval=5" % uuid)
         info("### Enabled lldp on openvswitch ###\n")
         # time.sleep(1)
         info("### Wakeup and verify lldp enable flag ###\n")
         out = s.cmd("ovs-vsctl list system | grep other_config")
-        assert 'lldp_enable="true"' in out, "lldp not enabled on switch " + str(switch_number)
-        info("### lldp configured correctly on switch %d ###\n" %switch_number)
-
+        assert 'lldp_enable="true"' in out, "lldp not enabled on switch " + \
+            str(switch_number)
+        info("### lldp configured correctly on switch %d ###\n"
+             % switch_number)
 
     #
     # Trigger a burst of events via ovs-appctl and make sure
     # it arrived by checking event counter
     #
-    def lldp_libevent (self, switch_number):
+
+    def lldp_libevent(self, switch_number):
         s = self.switch_variable(switch_number)
         self.pid_start = s.cmd("pidof ops-lldpd").strip()
         out = s.cmd("ovs-appctl -t ops-lldpd lldpd/test libevent 0")
-        assert "OK" in out, "switch %d libevent test init FAILED: %s" % (switch_number, out)
+        assert "OK" in out, "switch %d libevent test init FAILED: %s" \
+            % (switch_number, out)
         time.sleep(2)
         self.pid_end = s.cmd("pidof ops-lldpd").strip()
-        assert self.pid_start == self.pid_end, "lldpd restarted new pid %s" % self.pid_end
+        assert self.pid_start == self.pid_end, "lldpd restarted new pid %s" \
+            % self.pid_end
         info("### lldp process id is " + str(self.pid1) + " ###\n")
         out = s.cmd("ovs-appctl -t ops-lldpd lldpd/test libevent 1").strip()
-        assert "OK" in out, "switch %d libevent test FAILED: %s" % (switch_number, out)
+        assert "OK" in out, "switch %d libevent test FAILED: %s" \
+            % (switch_number, out)
         info("### switch %d libevent test PASSED ###\n" % (switch_number))
 
 
@@ -124,32 +130,32 @@ class lldpTest (OpsVsiTest):
 #
 class Test_lldp:
 
-    def setup (self):
+    def setup(self):
         pass
 
-    def teardown (self):
+    def teardown(self):
         pass
 
-    def setup_class (cls):
+    def setup_class(cls):
     # Create the Mininet topology based on mininet.
         Test_lldp.test_var = lldpTest()
         pass
 
-    def teardown_class (cls):
+    def teardown_class(cls):
         Test_lldp.test_var.net.stop()
 
-    def setup_method (self, method):
+    def setup_method(self, method):
         pass
 
-    def teardown_method (self, method):
+    def teardown_method(self, method):
         pass
 
-    def __del__ (self):
+    def __del__(self):
         del self.test_var
 
     # the actual test function
-    def test_lldp_full (self):
+    def test_lldp_full(self):
         info("\n########## Test lldpd burst of libevents ##########\n")
-        self.test_var.configure_lldp(1);
-        self.test_var.lldp_libevent(1);
+        self.test_var.configure_lldp(1)
+        self.test_var.lldp_libevent(1)
         # CLI(self.test_var.net)
--- ops/tests/test_lldpd_ct_counters_recovery.py
+++ ops/tests/test_lldpd_ct_counters_recovery.py
@@ -44,9 +44,10 @@ from opsvsiutils.systemutil import *
 # the ovs db such that it "seems" like it never crashed.
 #
 
+
 class myTopo(Topo):
 
-    def build (self, hsts=0, sws=2, **_opts):
+    def build(self, hsts=0, sws=2, **_opts):
 
         self.hsts = hsts
         self.sws = sws
@@ -58,37 +59,38 @@ class myTopo(Topo):
         # connect them together
         self.addLink('s1', 's2')
 
+
 class lldpTest (OpsVsiTest):
 
-    def setupNet (self):
+    def setupNet(self):
 
         # if you override this function, make sure to
         # either pass getNodeOpts() into hopts/sopts of the topology that
         # you build or into addHost/addSwitch calls
-        self.net = Mininet(topo=myTopo(hsts = 0, sws = 2,
-                                       hopts = self.getHostOpts(),
-                                       sopts = self.getSwitchOpts()),
-                                       switch = VsiOpenSwitch,
-                                       host = Host,
-                                       link = OpsVsiLink,
-                                       controller = None,
-                                       build = True)
+        self.net = Mininet(topo=myTopo(hsts=0, sws=2,
+                                       hopts=self.getHostOpts(),
+                                       sopts=self.getSwitchOpts()),
+                           switch=VsiOpenSwitch,
+                           host=Host,
+                           link=OpsVsiLink,
+                           controller=None,
+                           build=True)
 
-    def switch_variable (self, switch_number):
+    def switch_variable(self, switch_number):
         if (switch_number == 1):
             return self.net.switches[0]
         else:
             return self.net.switches[1]
 
-    def show_interface (self, switch_number, interface_number):
+    def show_interface(self, switch_number, interface_number):
         s = self.switch_variable(switch_number)
         command = "ovs-vsctl list interface " + str(interface_number)
         out = "\n\n### " + s.cmd(command) + " ###\n"
         info(out)
 
-    def is_interface_down (self, switch_number, interface_number):
-        info("### checking link state for switch %d interface %d: ###\n" \
-                % (switch_number, interface_number))
+    def is_interface_down(self, switch_number, interface_number):
+        info("### checking link state for switch %d interface %d: ###\n"
+             % (switch_number, interface_number))
         command = "ovs-vsctl list interface " + str(interface_number) + \
                   " | grep link_state | grep -v grep"
         s = self.switch_variable(switch_number)
@@ -100,9 +102,9 @@ class lldpTest (OpsVsiTest):
             info("### DOWN ###\n")
             return 1
 
-    def bring_interface_up (self, switch_number, interface_number):
-        info("\n### bringing interface %d up for switch %d ###\n" \
-                % (interface_number, switch_number))
+    def bring_interface_up(self, switch_number, interface_number):
+        info("\n### bringing interface %d up for switch %d ###\n"
+             % (interface_number, switch_number))
         s = self.switch_variable(switch_number)
         # set admin state & autoneg
         command = "ovs-vsctl -t 60 set interface " + str(interface_number) + \
@@ -110,18 +112,18 @@ class lldpTest (OpsVsiTest):
         s.cmd(command)
         # set connector
         command = "ovs-vsctl -t 60 set interface " + str(interface_number) + \
-                    " pm_info:connector=SFP_RJ45 " + \
-                    "pm_info:connector_status=supported"
+            " pm_info:connector=SFP_RJ45 " + \
+            "pm_info:connector_status=supported"
         s.cmd(command)
         output = "switch " + str(switch_number) + " interface " + \
                  str(interface_number)
         time.sleep(1)
         assert self.is_interface_down(switch_number, interface_number) == 0, \
             output + " did NOT come up\n"
-        output = "### " + output  + " is now successfully up ###\n"
+        output = "### " + output + "is now successfully up ###\n"
         info(output)
 
-    def lldp_process_restarted (self, switch_number):
+    def lldp_process_restarted(self, switch_number):
         s = self.switch_variable(switch_number)
         pid = s.cmd("pidof ops-lldpd").strip()
         if (switch_number == 1):
@@ -135,54 +137,57 @@ class lldpTest (OpsVsiTest):
             else:
                 return 1, pid
 
-    def configure_lldp (self, switch_number):
-        info("\n### enabling lldp globally for switch %d ###\n" % switch_number)
+    def configure_lldp(self, switch_number):
+        info("\n### enabling lldp globally for switch %d ###\n"
+             % switch_number)
         s = self.switch_variable(switch_number)
         if (switch_number == 1):
             self.pid1 = s.cmd("pidof ops-lldpd").strip()
             assert self.pid1 != "", "lldp process not running on switch " + \
-                          str(switch_number)
-            info("### lldp process id on switch " + \
-                str(switch_number) + \
-                " is " + \
-                str(self.pid1) + " ###\n")
+                str(switch_number)
+            info("### lldp process id on switch " +
+                 str(switch_number) +
+                 " is " +
+                 str(self.pid1) + " ###\n")
         else:
             self.pid2 = s.cmd("pidof ops-lldpd").strip()
             assert self.pid2 != "", "lldp process not running on switch " + \
-                          str(switch_number)
-            info("### lldp process id on switch " + \
-                str(switch_number) + \
-                " is " + \
-                str(self.pid2) + " ###\n")
-        uuid = s.cmd("ovs-vsctl list system | grep _uuid | " + \
+                str(switch_number)
+            info("### lldp process id on switch " +
+                 str(switch_number) +
+                 " is " +
+                 str(self.pid2) + " ###\n")
+        uuid = s.cmd("ovs-vsctl list system | grep _uuid | " +
                      "awk '{print $3}'").strip()
-        s.cmd("ovs-vsctl -t 60 set system %s " \
-              "other_config:lldp_enable=true " \
-              "other_config:lldp_mgmt_addr=204.152.189.%d " \
+        s.cmd("ovs-vsctl -t 60 set system %s "
+              "other_config:lldp_enable=true "
+              "other_config:lldp_mgmt_addr=204.152.189.%d "
               "other_config:lldp_tx_interval=5" % (uuid, switch_number))
         time.sleep(1)
         out = s.cmd("ovs-vsctl list system | grep other_config")
         assert 'lldp_enable="true"' in out, "lldp not enabled on switch " + \
-                str(switch_number)
-        info("### lldp configured correctly on switch %d ###\n" %switch_number)
+            str(switch_number)
+        info("### lldp configured correctly on switch %d ###\n"
+             % switch_number)
 
     #
     # get the numerical value of 'lldp_rx' statistic value
     #
-    def get_lldp_rx_count (self, switch_number, interface_number):
+    def get_lldp_rx_count(self, switch_number, interface_number):
         s = self.switch_variable(switch_number)
         # extract numerical value of (N) from string "lldp_rx=N,"
         command = "ovs-vsctl list interface " + str(interface_number) + \
                   " | grep lldp_statistics"
         out = s.cmd(command)
         for s in out.split():
-            if s.startswith('lldp_rx'): break
+            if s.startswith('lldp_rx'):
+                break
         s = s.replace('lldp_rx=', '')
         s = s.replace(',', '')
         lldp_rx_int_value = int(s)
         return lldp_rx_int_value
 
-    def wait_for_lldp_packets_exchange (self):
+    def wait_for_lldp_packets_exchange(self):
         info("\n### waiting for some lldp packets to be exchanged ###\n")
         count = 0
         while (count < 30):
@@ -197,55 +202,66 @@ class lldpTest (OpsVsiTest):
     #
     # verify that rx count is > after an lldp crash & recovery
     #
-    def verify_lldp_rx_count_after_crash (self, switch_number, interface_number):
-        info("\n### kill lldp on switch %d and verify rx cnts on intf %d ###\n" \
+
+    def verify_lldp_rx_count_after_crash(self,
+                                         switch_number,
+                                         interface_number):
+        info("\n### kill lldp on switch %d and verify rx cnts on intf %d ###\n"
              % (switch_number, interface_number))
         s = self.switch_variable(switch_number)
-        rxc_before_crash = self.get_lldp_rx_count(switch_number, interface_number)
+        rxc_before_crash = self.get_lldp_rx_count(switch_number,
+                                                  interface_number)
         s.cmd("killp ops-lldpd")
         time.sleep(2)
-        rxc_after_crash = self.get_lldp_rx_count(switch_number, interface_number)
+        rxc_after_crash = self.get_lldp_rx_count(switch_number,
+                                                 interface_number)
         assert rxc_after_crash >= rxc_before_crash, \
-            "switch %d interface %d test FAILED: before %d, after %d\n"  % \
-            (switch_number, interface_number, rxc_before_crash, rxc_after_crash)
-        info("### switch %d interface %d test PASSED: before %d, after %d ###\n" \
-            % (switch_number, interface_number, rxc_before_crash, rxc_after_crash))
+            "switch %d interface %d test FAILED: before %d, after %d\n" % \
+            (switch_number, interface_number,
+             rxc_before_crash, rxc_after_crash)
+        info("### switch %d interface %d test PASSED: before %d, after \
+            %d ###\n"
+             % (switch_number, interface_number,
+                rxc_before_crash, rxc_after_crash))
 
 #
 # final "Test_" class
 #
+
+
 class Test_lldp:
 
-    def setup (self):
+    def setup(self):
         pass
 
-    def teardown (self):
+    def teardown(self):
         pass
 
-    def setup_class (cls):
+    def setup_class(cls):
     # Create the Mininet topology based on mininet.
         Test_lldp.test_var = lldpTest()
         pass
 
-    def teardown_class (cls):
+    def teardown_class(cls):
         Test_lldp.test_var.net.stop()
 
-    def setup_method (self, method):
+    def setup_method(self, method):
         pass
 
-    def teardown_method (self, method):
+    def teardown_method(self, method):
         pass
 
-    def __del__ (self):
+    def __del__(self):
         del self.test_var
 
     # the actual test function
-    def test_lldp_full (self):
-        info("\n########## Test lldpd counter recovery after restart ##########\n")
-        self.test_var.configure_lldp(1);
-        self.test_var.configure_lldp(2);
-        self.test_var.bring_interface_up(1, 1);
-        self.test_var.bring_interface_up(2, 1);
-        self.test_var.wait_for_lldp_packets_exchange();
-        self.test_var.verify_lldp_rx_count_after_crash(1, 1);
-        self.test_var.verify_lldp_rx_count_after_crash(2, 1);
+    def test_lldp_full(self):
+        info("\n########## Test lldpd counter recovery after restart \
+            ##########\n")
+        self.test_var.configure_lldp(1)
+        self.test_var.configure_lldp(2)
+        self.test_var.bring_interface_up(1, 1)
+        self.test_var.bring_interface_up(2, 1)
+        self.test_var.wait_for_lldp_packets_exchange()
+        self.test_var.verify_lldp_rx_count_after_crash(1, 1)
+        self.test_var.verify_lldp_rx_count_after_crash(2, 1)
